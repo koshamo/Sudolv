e8bad60b47cbf4d331bdf6df00b35123bed3b760
koshamo <jochen.rassler@gmail.com>
2019-02-03 17:10:54 +0100

    Testfile justnumbers.txt and sudolv.lips with basic reading caps added

---
+++ src/sudolv.lisp
@@ -0,0 +1,8 @@
+(defun prompt-read ()
+  (format *query-io* "sudoku file (full path) : ")
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun read-sudoku (file)
+  (with-open-file (stream file)
+    (read stream)))
