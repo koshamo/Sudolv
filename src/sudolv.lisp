88482fae38f45836acdbd9c3c45b36368c6ae228
koshamo <jochen.rassler@gmail.com>
2019-02-13 19:42:00 +0100

    added implementation for single-position-solver (..in-line and ..in-col), and fixed array indexing

--- src/sudolv.lisp
+++ src/sudolv.lisp
@@ -57,56 +57,56 @@
 
 (defun replace-nils ()
   (every-cell *size*
-    (when (null (aref *sudoku* x y))
-      (setf (aref *sudoku* x y) (list-numbers *size*)))))
+    (when (null (aref *sudoku* y x))
+      (setf (aref *sudoku* y x) (list-numbers *size*)))))
 
 (defun list-numbers (num)
-  (list-numbers' num nil))
+  (list-numbers* num nil))
 
-(defun list-numbers' (num numlist)
+(defun list-numbers* (num numlist)
   (if (> num 0)
-      (list-numbers' (- num 1) (cons num numlist))
+      (list-numbers* (- num 1) (cons num numlist))
       numlist))
   
 (defun remove-possibilities (x y)
-  (let ((num (aref *sudoku* x y)))
+  (let ((num (aref *sudoku* y x)))
     (remove-possibilities-in-line num y)
     (remove-possibilities-in-col num x)
     (remove-possibilities-in-square num x y)))
 
 (defun remove-possibilities-in-line (num line)
   (dotimes (x *size*)
-    (when (listp (aref *sudoku* x line))
-      (setf (aref *sudoku* x line) (remove num (aref *sudoku* x line))))))
+    (when (listp (aref *sudoku* line x))
+      (setf (aref *sudoku* line x) (remove num (aref *sudoku* line x))))))
 
 (defun remove-possibilities-in-col (num col)
   (dotimes (y *size*)
-    (when (listp (aref *sudoku* col y))
-      (setf (aref *sudoku* col y) (remove num (aref *sudoku* col y))))))
+    (when (listp (aref *sudoku* y col))
+      (setf (aref *sudoku* y col) (remove num (aref *sudoku* y col))))))
 
 (defun remove-possibilities-in-square (num col line)
   (let ((square-x (truncate (/ col *square-size*)))
 	(square-y (truncate (/ line *square-size*))))
     (every-cell *square-size*
       (when (listp (aref *sudoku*
-			 (+ x (* square-x *square-size*))
-			 (+ y (* square-y *square-size*))))
+			 (+ y (* square-y *square-size*))
+			 (+ x (* square-x *square-size*))))
 	(setf (aref *sudoku*
-		    (+ x (* square-x *square-size*))
-		    (+ y (* square-y *square-size*)))
+		    (+ y (* square-y *square-size*))
+		    (+ x (* square-x *square-size*)))
 	      (remove num (aref *sudoku*
-				(+ x (* square-x *square-size*))
-				(+ y (* square-y *square-size*)))))))))
+				(+ y (* square-y *square-size*))
+				(+ x (* square-x *square-size*)))))))))
 
 (defun init-possibilities ()
   (every-cell *size*
-    (when (numberp (aref *sudoku* x y))
+    (when (numberp (aref *sudoku* y x))
       (remove-possibilities x y))))
 
 (defun sudoku-solved-p ()
   (let ((solved t))
     (every-cell *size*
-      (when (listp (aref *sudoku* x y))
+      (when (listp (aref *sudoku* y x))
 	(setf solved nil)
 	(return)))
     solved))
@@ -114,8 +114,8 @@
 (defun single-value-solver ()
   (let ((changed nil))
     (every-cell *size*
-      (when (and (listp (aref *sudoku* x y)) (eql (length (aref *sudoku* x y)) 1))
-	(setf (aref *sudoku* x y) (car (aref *sudoku* x y)))
+      (when (and (listp (aref *sudoku* y x)) (eql (length (aref *sudoku* y x)) 1))
+	(setf (aref *sudoku* y x) (car (aref *sudoku* y x)))
 	(remove-possibilities x y)
 	(setf changed t)))
     changed))
@@ -123,24 +123,73 @@
 (defun single-position-solver ()
   (progn
     (single-position-in-line)
+    (single-value-solver)
     (single-position-in-col)
-    (single-position-in-square)))
+    (single-value-solver)
+    (single-position-in-square)
+    (single-value-solver)))
 
 (defun single-position-in-line ()
-  )
+  (dotimes (y *size*)
+    (single-position-in-line* y)))
+
+(defun single-position-in-line* (line)
+  (dolist (num (list-numbers *size*))
+    (let ((cnt 0))
+      (dotimes (x *size*)
+	(when (listp (aref *sudoku* line x))
+	  (when (member num (aref *sudoku* line x))
+	    (incf cnt))))
+      (when (= cnt 1)
+	(let ((x (first-possibility-in-line num line)))
+	  (setf (aref *sudoku* line x) num)
+	  (remove-possibilities x line))))))
 
 (defun single-position-in-col ()
-  nil)
+  (dotimes (x *size*)
+    (single-position-in-col* x)))
+
+(defun single-position-in-col* (col)
+  (dolist (num (list-numbers *size*))
+    (let ((cnt 0))
+      (dotimes (y *size*)
+	(when (listp (aref *sudoku* y col))
+	  (when (member num (aref *sudoku* y col))
+	    (incf cnt))))
+      (when (= cnt 1)
+	(let ((y (first-possibility-in-col num col)))
+	  (setf (aref *sudoku* y col) num)
+	  (remove-possibilities col y))))))
 
 (defun single-position-in-square ()
   nil)
 
 
+(defun first-possibility-in-line (num line)
+  (let ((pos -1))
+    (dotimes (x *size*)
+      (when (listp (aref *sudoku* line x))
+	(when (member num (aref *sudoku* line x))
+	  (setf pos x)
+	  (return))))
+    (if (not (eql pos -1))
+	pos
+	nil)))
+
+(defun first-possibility-in-col (num col)
+  (let ((pos -1))
+    (dotimes (y *size*)
+      (when (listp (aref *sudoku* y col))
+	(when (member num (aref *sudoku* y col))
+	  (setf pos y)
+	  (return))))
+    (if (not (eql pos -1))
+	pos
+	nil)))
+
 
 (defun num-in-cell-p (num col line)
-  (if (eql (aref *sudoku* col line) num)
-      t
-      nil))
+  (eql (aref *sudoku* line col) num))
 
 (defun num-in-line (num line)
   (position-of-num num 0 line 'line))
